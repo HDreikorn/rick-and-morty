9ff984664e1e9357a1f0abbd0ff98034bb7f9647
HDreikorn <hillary@dreikorn.com>
2021-02-06 13:35:48 -0600

    added skeleton for tracking lists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import AppHeader from "./components/app-header";
+import EpisodeTrackingCard from "./components/episode-tracking-card";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <div>
+        <EpisodeTrackingCard title={"Watched"} />
+        <EpisodeTrackingCard title={"Favorites"} />
+        <EpisodeTrackingCard title={"Must-Watch"} />
+      </div>
     </div>
   );
 }

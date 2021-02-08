cdc9b96fa76c65d1009e1f5980ce5e452c3b31d8
HDreikorn <hillary@dreikorn.com>
2021-02-08 11:08:30 -0600

    started adding some tests [WIP]

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Default favorite list shows 'is empty' message", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const defaultList = screen.getByTestId("default-test-favorites");
+  expect(defaultList).toHaveTextContent(
+    "Nothing to see here...yet. Use search on left to add."
+  );
+});
+
+test("Default watched list shows 'is empty' message", () => {
+  render(<App />);
+  const defaultList = screen.getByTestId("default-test-watched");
+  expect(defaultList).toHaveTextContent(
+    "Nothing to see here...yet. Use search on left to add."
+  );
+});
+
+test("Default must-watch list shows 'is empty' message", () => {
+  render(<App />);
+  const defaultList = screen.getByTestId("default-test-must-watch");
+  expect(defaultList).toHaveTextContent(
+    "Nothing to see here...yet. Use search on left to add."
+  );
 });

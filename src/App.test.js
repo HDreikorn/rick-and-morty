d6304f4168b9a65e51d8ee947d7f1a488bfce447
HDreikorn <hillary@dreikorn.com>
2021-02-09 10:23:14 -0600

    learning just put in very simple tests [WIP]

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Default favorite list shows 'is empty' message", () => {
+test("Default favorite list shows 'is empty' message initially", () => {
   render(<App />);
   const defaultList = screen.getByTestId("default-test-favorites");
   expect(defaultList).toHaveTextContent(
@@ -9,7 +9,7 @@ test("Default favorite list shows 'is empty' message", () => {
   );
 });
 
-test("Default watched list shows 'is empty' message", () => {
+test("Default watched list shows 'is empty' message initially", () => {
   render(<App />);
   const defaultList = screen.getByTestId("default-test-watched");
   expect(defaultList).toHaveTextContent(
@@ -17,10 +17,14 @@ test("Default watched list shows 'is empty' message", () => {
   );
 });
 
-test("Default must-watch list shows 'is empty' message", () => {
+test("Default must-watch list shows 'is empty' message initially", () => {
   render(<App />);
   const defaultList = screen.getByTestId("default-test-must-watch");
   expect(defaultList).toHaveTextContent(
     "Nothing to see here...yet. Use search on left to add."
   );
 });
+
+// Other Things to Test (still learning how to):
+// Test the reducer action types
+// test graph ql fetches
